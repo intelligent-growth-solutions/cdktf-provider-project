33299778f8d7668c6880c2f19861572d4a0643ee
Melissa Quek <melissa.quek@intelligentgrowthsolutions.com>
2025-05-16 20:40:11 +0100

    Add gh tokens to force-release and provider-upgrade

--- src/index.ts
+++ src/index.ts
@@ -407,6 +407,7 @@ export class CdktfProviderProject extends cdk.JsiiProject {
         checkForUpgradesScriptPath: upgradeScript.path,
         workflowRunsOn,
         nodeHeapSize: maxOldSpaceSize,
+        npmInstallEnvVar: npmInstallEnvVar,
       });
       new AlertOpenPrs(this, {
         slackWebhookUrl: "${{ secrets.ALERT_PRS_SLACK_WEBHOOK_URL }}",
@@ -575,6 +576,15 @@ export class CdktfProviderProject extends cdk.JsiiProject {
     this.buildWorkflow?.addPostBuildSteps({
       name: "Revert package.json version bump",
       run: "git checkout package.json",
+    });
+
+    const forceReleaseWorkflow = this.tryFindObjectFile(
+      ".github/workflows/force-release.yml"
+    );
+    forceReleaseWorkflow?.addDeletionOverride("jobs.force_release_golang");
+    forceReleaseWorkflow?.addOverride(`jobs.force-release.env`, {
+      CI: "true",
+      GITHUB_TOKEN: `\${{ secrets.${npmInstallEnvVar} }}`,
     });
 
     new CopyrightHeaders(this);
