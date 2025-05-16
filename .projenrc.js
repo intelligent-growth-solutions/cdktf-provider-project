30ee60e0a8a94c283019539712af7605e29bcf95
Steven Butter <stevenbutter.451@gmail.com>
2025-05-16 12:47:44 +0100

    feat: upgrade grafana provider version

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,7 @@ const { CdktfProviderProject } = require('@cdktf/provider-project');
 const { Semver } = require('projen');
 
 const project = new CdktfProviderProject({
-  terraformProvider: "grafana@~> 3.7",
+  terraformProvider: "grafana@~> 3.25.0",
 });
 
 project.synth();
